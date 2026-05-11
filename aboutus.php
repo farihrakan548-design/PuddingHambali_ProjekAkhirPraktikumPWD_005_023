3d9ebe841144ccb39df35d527cfe19447bc11bda
farihrakan <farihrakan548@gmail.com>
2026-05-12 00:05:56 +0700

    page aboutus kurang gambar

--- aboutus.php
+++ aboutus.php
@@ -3,11 +3,67 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Pudding Hambali - About Us</title>
+    <title>About Us - Pudding Hambali</title>
+    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
   </head>
   <body>
-    <h1>About Us</h1>
+    <nav class="navbar navbar-expand-lg navbar-dark custom-navbar">
+    <div class="container">
+        <a class="navbar-brand fw-bold" href="#">🍮 Pudding Hambali</a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="products.php">Products</a></li>
+                <li class="nav-item"><a class="nav-link" href="payment.php">Payment</a></li>
+                <li class="nav-item"><a class="nav-link" href="aboutus.php">About Us</a></li>
+                <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
+                <li class="nav-item"><a class="nav-link" href="register.php">Register</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="container mt-5">
+
+    <div class="d-flex justify-content-center gap-4 flex-wrap">
+
+        <div class="card shadow" style="width: 18rem;">
+
+            <img src="PuddingHambali_ProjekAkhirPraktikumPWD_005_023/images/puddingcaramel.jpg"class="card-img-top"alt="...">
+            <div class="card-body">
+                <p class="card-text">
+                    Nama : Farih Rakan Abqori <br>
+                    NIM : 124250005 <br>
+                    Kelas : SI-A
+                </p>
+
+            </div>
+        </div>
+        <div class="card shadow" style="width: 18rem;">
+
+            <img src="PuddingHambali_ProjekAkhirPraktikumPWD_005_023/images/puddingcaramel.jpg"class="card-img-top"alt="...">
+
+            <div class="card-body">
+
+                <p class="card-text">
+                    Nama : Arsi Candra Kusuma <br>
+                    NIM : 124250023 <br>
+                    Kelas : SI-A
+                </p>
+
+            </div>
+        </div>
+
+    </div>
+
+</div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
   </body>
+
 </html>
